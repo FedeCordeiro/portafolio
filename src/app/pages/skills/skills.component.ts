d27c164f5bbeefb6684253a8548d895f6154d8bc
Fede Cordeiro <federico.cordeiro95@gmail.com>
2024-07-02 18:40:48 -0300

    skills and footer

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  items = [
+    {
+      name: 'Frontend',
+      tecnology: [
+        { name: 'HTML', src: './assets/icons/html.svg' },
+        { name: 'CSS', src: './assets/icons/css-3.svg' },
+        { name: 'Sass', src: './assets/icons/sass.svg' },
+        { name: 'Bootstrap', src: './assets/icons/bootstrap.svg' },
+        { name: 'JavaScript', src: './assets/icons/js2.svg' },
+        { name: 'Angular', src: './assets/icons/angular.svg' },
+      ],
+    },
+    {
+      name: 'Backend',
+      tecnology: [
+        { name: 'PHP', src: './assets/icons/php.svg' },
+        { name: 'Java', src: './assets/icons/java.svg' },
+        { name: 'MySQL', src: './assets/icons/mysql.svg' },
+        { name: 'Postgres', src: './assets/icons/postgres.svg' },
+      ],
+    },
+    {
+      name: 'Herramientas',
+      tecnology: [
+        { name: 'Git', src: './assets/icons/git.svg' },
+        { name: 'GitHub', src: './assets/icons/github.svg' },
+        { name: 'GitLab', src: './assets/icons/gitlab.svg' },
+        { name: 'VSCode', src: './assets/icons/vscode.svg' },
+        { name: 'Postman', src: './assets/icons/postman.svg' },
+        { name: 'Trello', src: './assets/icons/trello.svg' },
+        { name: 'Vercel', src: './assets/icons/vercel.svg' },
+      ],
+    },
+  ];
 }
