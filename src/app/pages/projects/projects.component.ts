65486ebd82f4485332fbea55fe5b2d037a3ca4e0
Fede Cordeiro <federico.cordeiro95@gmail.com>
2024-07-02 16:40:29 -0300

    contact and projects

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -26,6 +26,7 @@ export class ProjectsComponent {
       tecnology: [
         { name: 'HTML', src: './assets/icons/html.svg' },
         { name: 'CSS', src: './assets/icons/css-3.svg' },
+        { name: 'Bootstrap', src: './assets/icons/bootstrap.svg' },
         { name: 'JavaScript', src: './assets/icons/js2.svg' },
         { name: 'Angular', src: './assets/icons/angular.svg' },
       ],
@@ -42,6 +43,31 @@ export class ProjectsComponent {
       ],
       link: 'https://fedecordeiro.github.io/CineTrends.github.io/',
       github: 'https://github.com/FedeCordeiro/CineTrends.github.io'
+    },
+    {
+      name: 'Airline',
+      src: './assets/img/airline.jpg',
+      tecnology: [
+        { name: 'HTML', src: './assets/icons/html.svg' },
+        { name: 'CSS', src: './assets/icons/css-3.svg' },
+        { name: 'PHP', src: './assets/icons/php.svg' },
+        { name: 'MySQL', src: './assets/icons/mysql.svg' },
+      ],
+      link: ' ',
+      github: 'https://github.com/FedeCordeiro/Airline'
+    },
+    {
+      name: 'Airline API',
+      src: './assets/img/airline.jpg',
+      tecnology: [
+        { name: 'HTML', src: './assets/icons/html.svg' },
+        { name: 'CSS', src: './assets/icons/css-3.svg' },
+        { name: 'PHP', src: './assets/icons/php.svg' },
+        { name: 'MySQL', src: './assets/icons/mysql.svg' },
+        { name: 'Postman', src: './assets/icons/postman.svg' },
+      ],
+      link: ' ',
+      github: 'https://github.com/FedeCordeiro/AirlineAPI'
     },
   ]
 }
