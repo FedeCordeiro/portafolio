0f90624c11dc91b3a30e4ba543083d55e7b146af
Fede Cordeiro <federico.cordeiro95@gmail.com>
2024-09-12 18:55:34 -0300

    add age variable

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  birthDate: Date = new Date('1995-08-25'); //Fecha de nacimiento
+  age: number = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.calculateAge();
+  }
+
+  calculateAge(): void {
+    const today = new Date(); //Fecha actual
+    const birthDate = new Date(this.birthDate); //Fecha de nacimiento
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+
+    //Si aún no se cumplieron años, en el año actual
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+
+    this.age = age;
+  }
 
 }
